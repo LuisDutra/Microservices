8546a7fdc597e097ef76d3715261c01b3d3ac556
LuisDutra <luisvinicius99@gmail.com>
2022-06-24 16:51:34 -0300

    fix CommandsService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/commands/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+        
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controllers");
+        }
+    }
+}
